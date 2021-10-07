7a6275c7b5ada81b9661e8f8a61d1d3f5e7dfdbf
Carlos A Martinez <Linguz2@gmail.com>
2021-10-07 09:50:37 -0400

    Reverting to P0 status for posterity.

---
+++ WebUI/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+using Serilog;
+
+namespace WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IBL _bl;
+        public ProductController(IBL bl)
+        {
+            _bl = bl;
+        }
+
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+            List<CustomerVM> allCusto = _bl.GetAllCustomers()
+                                            .Select(c => new CustomerVM(c)).ToList();
+            return View(allCusto);
+            //List<Customer> allCustos = _bl.GetAllCustomers();
+            //return View(allCustos);
+        }
+
+        // GET: PopController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PopController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CustomerVM customer)
+        {
+            try
+            {
+                // if data is valid
+                if (ModelState.IsValid)
+                {
+                    if (customer.Name.Length == 1)
+                    {
+                        customer.Name = customer.Name.ToUpper();
+                    }
+                    else
+                    {
+                        customer.Name = customer.Name[0].ToString().ToUpper() + customer.Name.Substring(1).ToLower();
+                    }
+
+                    customer.Password = _bl.Hash(customer.Password);
+                    _bl.AddCustomer(customer.ToModel());
+                    return RedirectToAction(nameof(Index));
+                }
+                return View();
+            }
+            catch (Exception e)
+            {
+                Log.Warning("Creating customer failed.");
+                return View();
+            }
+        }
+
+        // GET: PopController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(new CustomerVM(_bl.GetCustomer(id)));
+        }
+
+        // POST: PopController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, CustomerVM customer)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    _bl.UpdateCustomer(customer.ToModel());
+                    return RedirectToAction(nameof(Index));
+                }
+                return RedirectToAction(nameof(Edit));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Edit));
+            }
+        }
+
+        // GET: PopController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(new CustomerVM(_bl.GetCustomer(id)));
+        }
+
+        // POST: PopController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                _bl.RemoveCustomer(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+    }
+}
---
+++ WebUI/Models/ProductVM.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Models;
+
+namespace WebUI.Models
+{
+    public class ProductVM
+    {
+        public ProductVM() { }
+
+        // constructor w/ Name
+        public ProductVM(string name)
+        {
+            this.Name = name;
+        }
+
+        public ProductVM(string name, int Price, string Description) : this(name)
+        {
+            this.Price = Price;
+            this.Description = Description;
+        }
+
+        public ProductVM(Product prod)
+        {
+            this.Id = prod.Id;
+            this.Name = prod.Name;
+            this.Price = prod.Price;
+            this.Description = prod.Description;
+        }
+
+        // properties
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Price: {this.Price}";
+        }
+    }
+}
